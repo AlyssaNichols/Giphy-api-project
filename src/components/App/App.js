fe1f345f1a0cd14f9f6459a33835f6b520717dcb
Alyssa Nichols <alyssa.s.nichols94@gmail.com>
2023-10-03 16:18:33 -0500

    image link is working to render on page load. Had to add contional rendering in so I didn't keep getting errors on page load for not being able to fing objects within the response.data

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useEffect } from "react";
 import axios from "axios";
-import './App.css';
+import "./App.css";
+import { useDispatch, useSelector } from "react-redux";
 
 function App() {
-  // Renders the entire app on the DOM
-  axios({
-    method: "GET",
-    url: "/random/",
-  })
-    .then((response) => {
-      console.log(response.data);
-      dispatch({ type: "SET_RANDOM", payload: response.data });
-    })
-    .catch((error) => {
-      console.log("error on GET", error);
-    });
+  const dispatch = useDispatch();
+  const random = useSelector((store) => store.random);
 
+  useEffect(() => {
+    getRandomGif();
+  }, []);
+
+  const getRandomGif = () => {
+    axios({
+      method: "GET",
+      url: "/random/",
+    })
+      .then((response) => {
+        console.log(response.data);
+        dispatch({ type: "SET_RANDOM", payload: response.data });
+      })
+      .catch((error) => {
+        console.log("error on GET", error);
+      });
+  };
+
+  // Check if random and its properties exist before rendering
+  if (!random || !random.data || !random.data.images || !random.data.images.original || !random.data.images.original.url) {
+    return (
+      <div>
+        <header className="App-header">
+          <h1>Random Giphy API</h1>
+          <button onClick={getRandomGif}>Refresh</button>
+        </header>
+        <p>Loading...</p>
+      </div>
+    );
+  }
 
   return (
     <div>
       <header className="App-header">
         <h1>Random Giphy API</h1>
+        <button onClick={getRandomGif}>Refresh</button>
       </header>
-      
-      <p>Results go here</p>
+      <img src={random.data.images.original.url} alt="Random Gif" />
+      <br />
+     
     </div>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './components/App/App.js';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
+import { legacy_createStore as createStore, combineReducers, applyMiddleware } from 'redux';
 // Provider allows us to use redux within our react app
 import { Provider } from 'react-redux';
 import logger from 'redux-logger';
